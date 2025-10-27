63f8db7e81276cbddeba1f574a39e767379fb423
Avi Fisher <avifisher82@gmail.com>
2025-10-27 15:06:27 +0200

    creat a 'creat_matrix' function

---
+++ submarines/board.py
@@ -0,0 +1,13 @@
+def creat_matrix(num):
+    matrix = []
+
+    for i in range(num):
+        row = []
+        for j in range(num):
+            row.append(0)
+
+        matrix.append(row)
+    return matrix
+
+
+
