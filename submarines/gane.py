c46b2a44d282a04a44411ec54af5d9128d10f5bb
Avi Fisher <avifisher82@gmail.com>
2025-10-27 16:38:36 +0200

    add a 'init_game' function

---
+++ submarines/gane.py
@@ -0,0 +1,33 @@
+def init_game(size,n_ships,max_shots,):
+
+    info_game = {"size":0,
+                 "ships":[],
+                 "shots":[],
+                 "n_ships":0,
+                 "max_shots":0,
+                 "shots_used":0}
+
+    return info_game
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
